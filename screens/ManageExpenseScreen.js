1c1d5481d49c0380c109ae0cbe91d7fa85407368
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-24 14:48:07 +0100

    Screen Setup and navigations packages installation

---
+++ screens/ManageExpenseScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ManageExpensesScreen() {
+  return (
+    <View>
+      <Text>ManageExpensesScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/RecentExpensesScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function RecentExpenseScreen() {
+  return (
+    <View>
+      <Text>RecentExpenseScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
