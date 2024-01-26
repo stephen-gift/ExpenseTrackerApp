ca5225a5518b02c00487e3589552f618802f8c8a
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-26 18:19:40 +0100

    update Fecth POST

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -43,32 +43,7 @@ export default function ManageExpensesScreen({ route, navigation }) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      fetch(
-        "https://stephen-expensetracker-default-rtdb.europe-west1.firebasedatabase.app/expense.json",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(expenseData),
-        }
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(
-              `Request failed with status code ${response.status}`
-            );
-          }
-          return response.json();
-        })
-        .then((data) => {
-          return data; // You can process the response data if needed
-        })
-        .catch((error) => {
-          console.error("Error storing expense data:", error.message);
-          throw error; // You can choose to rethrow the error or handle it accordingly
-        });
+      storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
--- util/http.js
+++ util/http.js
@@ -6,9 +6,36 @@ import axios from "axios";
 //   );
 // }
 
+// export function storeExpense(expenseData) {
+//   // Assuming you are using Firebase Realtime Database
+//   const firebaseEndpoint =
+//     "https://stephen-expensetracker-default-rtdb.europe-west1.firebasedatabase.app/expense.json";
+//   axios.post(firebaseEndpoint, expenseData);
+// }
+
 export function storeExpense(expenseData) {
-  // Assuming you are using Firebase Realtime Database
-  const firebaseEndpoint =
-    "https://stephen-expensetracker-default-rtdb.europe-west1.firebasedatabase.app/expense.json";
-  axios.post(firebaseEndpoint, expenseData);
+  fetch(
+    "https://stephen-expensetracker-default-rtdb.europe-west1.firebasedatabase.app/expense.json",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expenseData),
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Request failed with status code ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data; // You can process the response data if needed
+    })
+    .catch((error) => {
+      console.error("Error storing expense data:", error.message);
+      throw error; // You can choose to rethrow the error or handle it accordingly
+    });
 }
